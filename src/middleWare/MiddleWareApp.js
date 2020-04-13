4fa89cace2a101657789766c90a34535bd0b5ebe
Artyom <artyom.zayarny@gmail.com>
2020-04-13 23:06:45 +0300

    Init middleWareApp

---
+++ src/middleWare/MiddleWareApp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import List from './components/List'
+import Content from './components/Content'
+
+const MiddleWareApp = () => {
+    return (
+        <div>
+         <List />
+         <Content />
+        </div>
+    );
+};
+
+export default MiddleWareApp;
