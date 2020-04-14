a242fb6ae925d024a8efa6da401936333ed093da
Artyom <artyom.zayarny@gmail.com>
2020-04-14 21:55:39 +0300

    added redux devtools, thunk

--- src/middleWare/redux/store.js
+++ src/middleWare/redux/store.js
@@ -1,7 +1,15 @@
-import {createStore} from "redux";
+import {createStore,compose,applyMiddleware} from "redux";
+import thunk from 'redux-thunk'
+
+
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const store = createStore( (state) => {
     return state
-});
+},
+composeEnhancers(
+    applyMiddleware(thunk)
+)
+);
 
 export default store
