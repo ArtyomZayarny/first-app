43d5c21714bb6d98713013a10951e66ad7c6df13
artem <artyom.zayarny@gmail.com>
2020-04-28 22:40:27 +0300

    init mongoos

--- src/social-network/server/server.js
+++ src/social-network/server/server.js
@@ -1,18 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose')
+
+const bodyParser = require('body-parser');
+const userRoute = require('./routes/users')
 
 const port = 5000;
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Hellow World')
-})
+app.use(userRoute)
 
-app.listen(port, (err) => {
-    if (err) {
-        console.log(err);
+async function start() {
+    try {
+        await mongoose.connect(
+            'mongodb+srv://social:123@cluster0-fo52v.mongodb.net/todos', {
+            useNewUrlParser:true,
+            useFindAndModify:false,
+            useUnifiedTopology: true
+        })
+        app.listen(port, (err) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(`Server  is running on ${port} port`);
+            
+        })
+    } catch (e) {
+        console.log(e);
         
     }
-    console.log(`Server  is running on ${port} port`);
-    
-})
+}
+start()
+
+
